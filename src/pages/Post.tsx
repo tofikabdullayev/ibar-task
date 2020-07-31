1a48f6b353c70bbbfdaa69fb0c6f89849d8e459d
Tofik Abdullayev <abdullayevtofik@gmail.com>
2020-07-31 14:25:48 +0400

    *

---
+++ src/pages/Post.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const Post = (props: Props) => {
+  return <div>Selected post</div>;
+};
+
+export default Post;
---
+++ src/pages/User.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface Props {}
+
+const User = (props: Props) => {
+  return <div>User info</div>;
+};
+
+export default User;
